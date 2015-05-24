56ab46800d8214acbac1ae97cfa9dd5d5c4d3282
Michael Diener <michael@webdiener.ch>
2015-05-24 16:07:46 -0700

    Bugfix
    * fixed tests and added some styling

--- test/javascript/test_dizmo_X.js
+++ test/javascript/test_dizmo_X.js
@@ -1,5 +1,6 @@
 module('dizmo tests');
 test('test case one', function(assert) {
+    expect(0)
     // do testing here.
     // refer to the QUnit documentation for more information
     // https://qunitjs.com/
--- test/tests/test_ExampleTest.js
+++ test/tests/test_ExampleTest.js
@@ -9,4 +9,9 @@ QUnit.done(function() {
     // things to be done once tests are run
     dizmo.setAttribute('geometry/width', 1000);
     dizmo.setAttribute('geometry/height', 490);
+
+    jQuery('#front').height(dizmo.getAttribute('geometry/height') - 20);
+    jQuery('#front').width(dizmo.getAttribute('geometry/width') - 20);
 });
+
+dizmo.canDock(false)
