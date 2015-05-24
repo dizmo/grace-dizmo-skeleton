ba946d5800fccb9688bd7ed24b1c7c2141869d69
Michael Diener <michael@webdiener.ch>
2015-05-24 16:01:07 -0700

    Improvement
    * stripped further down to be a complete barebone project

--- src/application.js
+++ src/application.js
@@ -1,17 +1,13 @@
-// Needed for the dizmo menu to work
+// If your dizmo has a back side, include this function. Otherwise you
+// can delete it!
 function showBack() {
     dizmo.showBack();
 }
 
-// Needed for the dizmo menu to work
-function showFront() {
-    dizmo.showFront();
-}
-
-// Helper object to attach all the events to
-var events = {};
-
 // As soon as the dom is loaded, and the dizmo is ready, instantiate the main class
 window.document.addEventListener('dizmoready', function() {
     // Your code should be in here so that it is secured that the dizmo is fully loaded
+    document.getElementById('doneBtn').onclick = function() {
+        dizmo.showFront();
+    }
 });
